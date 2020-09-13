82311df22add7122428b98e8ca45a272c0d12380
Ten <ten@mutate.space>
2020-09-13 21:21:33 +0200

    feat: more content

--- src/components/LazyIframe.tsx
+++ src/components/LazyIframe.tsx
@@ -3,7 +3,7 @@ import { useInView } from "react-intersection-observer";
 export const LazyIframe: React.FC<
   { fallback?: React.ReactNode } & React.HTMLProps<HTMLIFrameElement>
 > = (props) => {
-  const { fallback, ...iframeProps } = props;
+  const { fallback, className, ...iframeProps } = props;
 
   const [ref, visible] = useInView();
 
@@ -12,8 +12,8 @@ export const LazyIframe: React.FC<
   }
 
   return (
-    <div ref={ref}>
-      <iframe {...iframeProps} />
+    <div ref={ref} className={className}>
+      <iframe className="w-full h-full" {...iframeProps} />
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import mapValues from "lodash/mapValues";
 import { BoundingMode, BotFeature, PoolProps } from "../logotype/types";
 import { PoolWithContent, Layout } from "../components/PoolWithContent";
 import { LazyIframe } from "../components/LazyIframe";
+import clsx from "clsx";
 
 const poolPropsSet = {
   backgroundColor: ["#CDCEDF", "#00F", "#000", "#D0F"],
@@ -17,13 +18,15 @@ const poolPropsSet = {
   botFeatures: [undefined, [BotFeature.Legs, BotFeature.Solid]],
 };
 
-const usePoolPropAtIndex = (i: number): PoolProps =>
+const poolProps = (i: number): PoolProps =>
   mapValues(poolPropsSet, (arr) => arr[i]);
+
+const clsSectionTitle = "text-5xl pt-6 mb-6 text-center";
 
 const Page = () => (
   <main>
     <PoolWithContent
-      poolProps={usePoolPropAtIndex(0)}
+      poolProps={poolProps(0)}
       layout={Layout.Full}
       classNames={{ content: "pointer-events-none" }}
     >
@@ -36,7 +39,7 @@ const Page = () => (
         </div>
       </div>
     </PoolWithContent>
-    <PoolWithContent poolProps={usePoolPropAtIndex(1)} layout={Layout.Half}>
+    <PoolWithContent poolProps={poolProps(1)} layout={Layout.Half}>
       <p className="text-2xl mb-6">
         I am an experienced web developer who is passionate about design on the
         web as a multi-media medium.
@@ -48,21 +51,36 @@ const Page = () => (
       </p>
     </PoolWithContent>
     <section>
-      <h2 className="text-5xl pt-6 mb-6 text-center">Featured Projects</h2>
-      <div className="grid grid-cols-2">
+      <h2 className={clsSectionTitle}>Featured Projects</h2>
+      <div className="grid md:grid-cols-2">
         <div className="p-4 text-4xl self-center">
+          <h3 className="mb-2">Wurstgang / Yee Dog</h3>
           <p>
             I made a computer dog who can be your desktop companion. And you can
             feed him your files*.
           </p>
-          <a
-            className="a text-3xl"
-            href="https://yee.dog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Visit The Dog
-          </a>
+          <ul className="grid md:grid-cols-2 text-3xl">
+            <li>
+              <a
+                className="a"
+                href="https://yee.dog/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit The Dog
+              </a>
+            </li>
+            <li>
+              <a
+                className="a"
+                href="https://mutate.space/projects/wurstgang/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Case Study
+              </a>
+            </li>
+          </ul>
           <p className="mt-8 text-xs">
             *drag & drop, no files are uploaded or deleted.
           </p>
@@ -72,38 +90,99 @@ const Page = () => (
           style={{ minHeight: "50vh" }}
           src="https://yee.dog/"
           fallback={
-            <div className="w-full bg-gray-300" style={{ minHeight: "50vh" }} />
+            <div className="w-full bg-gray-200" style={{ minHeight: "50vh" }} />
           }
         />
       </div>
     </section>
-    <section className="grid grid-cols-2">
-      <LazyIframe
-        className="w-full"
-        style={{ minHeight: "50vh" }}
-        src="https://dualai.com/"
-        fallback={
-          <div className="w-full bg-gray-300" style={{ minHeight: "50vh" }} />
-        }
-      />
+    <section className="grid md:grid-cols-2">
+      <video autoPlay muted loop>
+        <source src="/assets/dualai-demo.mp4" />
+      </video>
       <div className="p-4 text-4xl self-center">
+        <h3 className="mb-2">Website for Dualai Design Studio</h3>
         <p>
           I conceptualized and developed a website for Dualai Design Studio. I
           attempted to use 3D to demonstrate the tangibility of the studio's
           specialized editorial design.
         </p>
-        <a
-          className="a text-3xl"
-          href="https://dualai.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit the site
-        </a>
+        <ul className="grid md:grid-cols-2 text-3xl">
+          <li>
+            <a
+              className="a"
+              href="https://dualai.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit the site
+            </a>
+          </li>
+          <li>
+            <a
+              className="a"
+              href="https://mutate.space/projects/dualai-design-studio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Case Study
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
-    <section>
-      <h2 className="text-5xl pt-6 mb-6 text-center">Experiences</h2>
+    <PoolWithContent layout={Layout.Full} poolProps={poolProps(2)}>
+      <div className="flex items-center h-full">
+        <div className="w-full">
+          <h2 className={clsx(clsSectionTitle, "text-white")}>More Projects</h2>
+          <ul className="text-3xl max-w-lg mx-auto p-4 text-center text-white">
+            <li className="my-4">A data-moshing like Unity effect</li>
+            <li className="my-4">Reactive-diffusion web toy</li>
+            <li className="my-4">
+              Some fun GIFs I made when I was in university
+            </li>
+            <li className="my-4">Illustrations & drawings</li>
+            <li className="my-4">...</li>
+            <li className="my-4">
+              <a
+                className="px-2 hover:bg-white hover:text-black"
+                href="https://mutate.space"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Check them out on mutate.space
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </PoolWithContent>
+    <section className="mb-16">
+      <div className="flex justify-center my-8">
+        <div className="bg-blue-500 w-16 h-32 transform translate-x-2"></div>
+        <div className="bg-blue-500 w-32 h-32 rounded-full"></div>
+      </div>
+      <div className="mt-8 grid grid-cols-4 text-2xl text-center">
+        <span>
+          <a className="a" href="https://www.linkedin.com/in/hytien/">
+            LinkedIn
+          </a>
+        </span>
+        <span>
+          <a className="a" href="mailto:hytien.10@gmail.com">
+            Email
+          </a>
+        </span>
+        <span>
+          <a className="a" href="https://github.com/fillmember">
+            GitHub
+          </a>
+        </span>
+        <span>
+          <a className="a" href="https://twitter.com/fillmember">
+            Twitter
+          </a>
+        </span>
+      </div>
     </section>
   </main>
 );
