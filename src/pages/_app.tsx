0154e241ec73a8b1a562bb98d9ec249d06feb14f
Ten <ten@mutate.space>
2020-09-13 21:38:03 +0200

    feat: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
-import "../style/common.css";
+import Head from "next/head";
 import { IconContext } from "react-icons";
+import "../style/common.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <IconContext.Provider value={{ className: "inline" }}>
+      <Head>
+        <title>H10</title>
+        <link rel="icon" type="image/svg" href="/favicon.svg"></link>
+      </Head>
       <Component {...pageProps} />
     </IconContext.Provider>
   );
