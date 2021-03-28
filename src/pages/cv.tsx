598eae3533b3a776f97f88f47b1d631052827d2e
fillmember <fillmember@gmail.com>
2021-03-28 10:06:00 +0200

    update cv

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,24 +1,31 @@
+import Head from "next/head";
 import styles from "../style/cv.module.css";
 
-const email = "fillmember@gmail.com";
-const obfuscated = email
-  .split("")
-  .map((char) => `&#${char.charCodeAt(0)};`)
-  .join("");
+const obfuscate = (str: string): string =>
+  str
+    .split("")
+    .map((char) => `&#${char.charCodeAt(0)};`)
+    .join("");
+const email = obfuscate("fillmember@gmail.com");
 const print = () => window.print();
 
 export default function CV() {
   return (
     <main className={styles.cv}>
+      <Head>
+        <title>CV - H10</title>
+      </Head>
       <button className="p-2 absolute top-4 right-4 border border-gunsmoke-500 print:hidden" onClick={print}>
         download as pdf
       </button>
       <section>
         <h1>Hui-Yuan Tien</h1>
-        <p className="grid grid-cols-2 max-w-lg">
-          <a href={`mailto:${obfuscated}`} dangerouslySetInnerHTML={{ __html: obfuscated }}></a>
-          <a href="//github.com/fillmember">github.com/fillmember</a>
-        </p>
+        <nav className="text-sm grid grid-cols-2 md:grid-cols-4 gap-x-8 gap-y-1">
+          <a href="https://ten10.netlify.app/">ten10.netlify.app</a>
+          <a href={`mailto:`} dangerouslySetInnerHTML={{ __html: email }}></a>
+          <a href="https://github.com/fillmember">github.com/fillmember</a>
+          <a href="https://www.linkedin.com/in/hytien/">www.linkedin.com/in/hytien/</a>
+        </nav>
       </section>
       <div className="grid grid-cols-2 gap-8 mt-8">
         <section>
@@ -28,87 +35,90 @@ export default function CV() {
             <span> @ </span>
             <span>Tinka</span>
           </h3>
+          <dl>
+            <dt>2019 ~ present</dt>
+            <dt>Zwolle, NL</dt>
+          </dl>
           <p>
-            <span>2019 ~ present</span>
-            <span>Zwolle, NL</span>
-            <span className={styles.text}>
-              Working on streamlining the development and design process to unify the UI/UX offers and wrangle for clarity in Tinka's
-              various products.
-            </span>
+            Working on streamlining the development and design process to unify the UI/UX offers and wrangle for clarity in Tinka's various
+            products.
           </p>
           <h3>
             <span>Front-end developer</span>
             <span> @ </span>
             <span>hyam Studios</span>
           </h3>
-          <p>
-            <span>2018 ~ 2019</span>
-            <span>Berlin, DE</span>
-            <span className={styles.text}>
-              Consult clients and outline technical solutions, shaping the design studio's digitial offers.
-            </span>
-          </p>
+          <dl>
+            <dt>2018 ~ 2019</dt>
+            <dt>Berlin, DE</dt>
+          </dl>
+          <p>Consult clients and outline technical solutions, shaping the design studio's digitial offers.</p>
           <h3>
             <span>Creative developer</span>
             <span> @ </span>
             <span>Artificial Rome</span>
           </h3>
-          <p>
-            <span>2016 ~ 2018</span>
-            <span>Berlin, DE</span>
-            <span className={styles.text}>explore while building epic landscape using edge-cutting technologies</span>
-          </p>
+          <dl>
+            <dt>2016 ~ 2018</dt>
+            <dt>Berlin, DE</dt>
+          </dl>
+          <p>explore while building epic landscape using edge-cutting technologies</p>
         </section>
         <section>
           <h2>Skills</h2>
           <h3>Tech</h3>
           <ul className="grid grid-cols-2">
-            <li>TypeScript</li>
-            <li>React</li>
-            <li>Vue</li>
-            <li>Tailwind CSS</li>
-            <li>Three</li>
-            <li>react-fiber-three</li>
-            <li>C#</li>
-            <li>Unity</li>
-            <li>GLSL</li>
-            <li>API Design</li>
+            <span>TypeScript</span>
+            <span>React</span>
+            <span>Vue</span>
+            <span>Tailwind CSS</span>
+            <span>Three</span>
+            <span>react-fiber-three</span>
+            <span>C#</span>
+            <span>Unity</span>
+            <span>GLSL</span>
+            <span>API Design</span>
           </ul>
           <h3>Design</h3>
           <ul className="grid grid-cols-2">
-            <li>Sketch</li>
-            <li>User Experience</li>
-            <li>User Interface</li>
-            <li>Animation</li>
+            <span>Sketch</span>
+            <span>User Experience</span>
+            <span>User Interface</span>
+            <span>Animation</span>
+          </ul>
+          <h3>Language</h3>
+          <ul className="grid grid-cols-2">
+            <li>Mandarin (native)</li>
+            <li>English (fluent)</li>
+            <li>German (B1)</li>
+            <li>Dutch (A2)</li>
           </ul>
         </section>
         <section>
           <h2>Projects</h2>
           <h3>
-            <span>Yee Dog</span> <a href="https://yee.dog">yee.dog</a>
+            <span className="mr-4">Yee Dog</span> <a href="https://yee.dog">yee.dog</a>
           </h3>
-          <p>
-            <span className="col-span-2">Virtual dog companion available 24/7.</span>
-          </p>
+          <p>Virtual dog companion available 24/7.</p>
           <h3>
-            <span>Dualai</span> <a href="https://dualai.com">dualai.com</a>
+            <span className="mr-4">Dualai</span> <a href="https://dualai.com">dualai.com</a>
           </h3>
-          <p>
-            <span className="col-span-2">3D portfolio for TW-based design studio.</span>
-          </p>
+          <p>3D portfolio for TW-based design studio.</p>
         </section>
         <section>
           <h2>Education</h2>
           <h3>Master of Animation</h3>
-          <p>
-            <span>2014 ~ 2015</span>
+          <dl>
             <span>Avans Hogeschool</span>
-          </p>
+            <span className="text-right">2014 ~ 2015</span>
+            <span>Breda, NL</span>
+          </dl>
           <h3>Bachelor of Design</h3>
-          <p>
-            <span>2009 ~ 2013</span>
+          <dl>
             <span>National Taiwan University of Science and Technology</span>
-          </p>
+            <span className="text-right">2009 ~ 2013</span>
+            <span>Taipei, TW</span>
+          </dl>
         </section>
       </div>
     </main>
